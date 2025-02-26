d484b61be607e3cab03fb59b30a5d3ef4dc9861d
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-02-26 14:15:57 +0800

    Add Home Button on NotFoundPage. Add minor styling.

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
+
 export default function NotFoundPage() {
   return (
     <div className='bg-[url("/img/rizky-subagja-1k7TnX5GAww-unsplash.jpg")] h-screen bg-cover flex items-center justify-center'>
-      <div className="w-[850px] h-[600px] px-8 gap-8 bg-off-white/10 backdrop-blur-md bg-linear-to-br from-off-white/10 to-[#666666]/10  rounded-[50px] flex items-center justify-center flex-col">
+      <div className="border border-off-white w-[850px] h-[600px] px-8 gap-8 bg-off-white/10 backdrop-blur-md bg-linear-to-br from-off-white/10 to-[#666666]/10  rounded-[50px] flex items-center justify-center flex-col">
         <h1 className="text-off-white text-6xl text-center leading-[57px] tracking-tighter">
           Hold Tight-We're Building Something Cool!
         </h1>
@@ -9,6 +12,13 @@ export default function NotFoundPage() {
           This page is currently under construction, but we’re working hard to
           make it worth the wait. Stay tuned for updates!
         </p>
+        <Button
+          asChild
+          variant="outline"
+          className="w-[150px] text-off-white border-off-white hover:bg-off-white/20"
+        >
+          <Link to="/">Home</Link>
+        </Button>
       </div>
     </div>
   );
