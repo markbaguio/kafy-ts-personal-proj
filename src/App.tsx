55ec2d654bf1b70dd839cf7294beadbfc902aac9
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-02-28 13:50:23 +0800

    WIP: Add footer. Add icon button and size in shadcn button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import AuthPageHeader from "./components/common/AuthPageHeader";
+import Footer from "./components/common/Footer";
 import PageHeader from "./components/common/PageHeader";
 
 function App() {
   return (
     <>
       <PageHeader />
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum, eius. Sit
+        maiores repellat animi voluptatum aliquam quo pariatur? Error mollitia
+        veritatis illo qui culpa. Iste, corrupti quisquam porro quasi ea sed
+        natus, laborum enim reiciendis facere expedita voluptatibus sapiente, a
+        temporibus quidem! Sapiente eos voluptatem dolorem ducimus vitae, sit
+        ab?
+      </div>
+      <Footer />
     </>
   );
 }
---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,42 @@
+import { Facebook, FacebookIcon } from "lucide-react";
+import Logo from "./Logo";
+import { Button } from "../ui/button";
+import { Link } from "react-router";
+
+export default function Footer() {
+  return (
+    <footer className="bg-milky-white flex flex-col-reverse md:flex-row">
+      <div className="flex flex-col w-full items-start p-3 gap-2 md:w-[40%]">
+        <Logo />
+        <div className="">
+          <p className="text-sm font-light">
+            &copy; 2025 Mark Godwin C. Baguio. All rights reserved
+          </p>
+          <p className="text-sm font-light">
+            Fueled by ☕ & passion. Thanks for stopping by!
+          </p>
+        </div>
+        <hr className="border-raisin-black w-full" />
+        <div className="flex">
+          <Button asChild className="rounded-full" variant="icon" size="icon">
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Facebook className="size-full" />
+            </a>
+          </Button>
+        </div>
+      </div>
+      <div className="bg-light-caramel w-full md:w-[60%]">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus
+        architecto esse assumenda, excepturi, officiis quo placeat tempore
+        delectus unde ratione temporibus expedita alias! Culpa debitis accusamus
+        reprehenderit quaerat ut dicta amet dolor vero est provident cupiditate
+        neque praesentium animi qui, veniam autem libero nam quam repellat
+        cumque ipsam. Sit, illo.
+      </div>
+    </footer>
+  );
+}
