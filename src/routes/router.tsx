9522c7ed41460ab49b65e7f6b834222f27d872bb
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-03-03 14:15:48 +0800

    Refactor: add a dedicated router filer.

---
+++ src/routes/router.tsx
@@ -0,0 +1,33 @@
+import AuthLayout from "@/layouts/AuthLayout";
+import MainLayout from "@/layouts/MainLayout";
+import GiftCards from "@/pages/GiftCards";
+import Homepage from "@/pages/Homepage";
+import { Menu } from "@/pages/Menu";
+import NotFoundPage from "@/pages/NotFoundPage";
+import Rewards from "@/pages/Rewards";
+import { SignIn } from "@/pages/SignIn";
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      { index: true, element: <Homepage /> },
+      { path: "/menu", element: <Menu /> },
+      { path: "/rewards", element: <Rewards /> },
+      {
+        path: "/gift",
+        element: <GiftCards />,
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [{ path: "signin", element: <SignIn /> }],
+  },
+]);
+
+export default router;
