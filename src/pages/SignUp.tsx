9829cfcfdb4a54212b1477bd10fa2019a3c966c0
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-03-06 15:40:23 +0800

    Change font file/s storage to public folder. WIP: Change sign up after pseudo border opacity. Fix font src url.

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -79,7 +79,7 @@ export default function SignUp() {
             </div>
           </CardContent>
           <CardFooter className="bg-milky-white w-full flex flex-col gap-6 items-center justify-center">
-            <div className="w-full after:border-raisin-black relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t font-light">
+            <div className="w-full after:border-raisin-black/50 relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t font-light">
               <span className="bg-milky-white relative z-10 px-2">
                 Or sign up with
               </span>
