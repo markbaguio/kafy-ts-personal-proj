f632c5f9e00a6f2d18647bf5d5a80bca798169d7
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-03-18 14:44:39 +0800

    bugfix: fix not found page(404 page) responsive sizing.

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,14 +1,18 @@
+import Logo from "@/components/common/Logo";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router";
 
 export default function NotFoundPage() {
   return (
-    <div className='bg-[url("/img/rizky-subagja-1k7TnX5GAww-unsplash.jpg")] h-screen bg-cover flex items-center justify-center'>
-      <div className="border border-off-white w-[850px] h-[600px] px-8 gap-8 bg-off-white/10 backdrop-blur-md bg-linear-to-br from-off-white/10 to-[#666666]/10  rounded-[50px] flex items-center justify-center flex-col">
-        <h1 className="text-off-white text-6xl text-center leading-[57px] tracking-tighter">
+    <div className='bg-[url("/img/rizky-subagja-1k7TnX5GAww-unsplash.jpg")] p-5 md:p-20 h-screen bg-cover flex flex-col gap-10 items-center justify-center'>
+      <Link to="/">
+        <Logo darkBg classname="text-9xl" />
+      </Link>
+      <div className="border border-off-white w-full max-w-[850px] h-fit py-15 px-6 gap-5 md:gap-8 bg-off-white/10 backdrop-blur-md bg-linear-to-br from-off-white/10 to-[#666666]/10  rounded-[50px] flex items-center justify-center flex-col">
+        <h1 className="text-off-white text-2xl md:text-4xl lg:text-6xl text-center md:leading-[57px] tracking-tighter">
           Hold Tight-We're Building Something Cool!
         </h1>
-        <p className="text-off-white text-center font-light text-2xl leading-[57px] tracking-tighter">
+        <p className="text-off-white text-center font-light text-sm md:text-xl lg:text-2xl md:Lleading-[57px] tracking-tighter">
           This page is currently under construction, but we’re working hard to
           make it worth the wait. Stay tuned for updates!
         </p>
