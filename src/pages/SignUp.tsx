a25b40f20a0368edc457f95228b21b2e6f58af46
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-03-31 14:04:38 +0800

    WIP: remove unused lines

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -16,8 +16,6 @@ import { useAuth } from "@/hooks/useAuth";
 import { useNavigate } from "react-router";
 import { toast } from "sonner";
 import {
-  AuthApiError,
-  AuthRetryableFetchError,
   isAuthApiError,
   isAuthRetryableFetchError,
 } from "@supabase/supabase-js";
