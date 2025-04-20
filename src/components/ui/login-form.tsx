17929c735bd6c39c40e1712126fdcc56e2f4653a
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-04-20 22:03:16 +0800

    WIP: add additional ping on sign in loading.

--- src/components/ui/login-form.tsx
+++ src/components/ui/login-form.tsx
@@ -100,8 +100,11 @@ export function LoginForm({
   if (signInMutation.isPending) {
     return (
       <section>
-        <div className="flex flex-col gap-8 h-screen items-center justify-center text-raisin-black">
-          <div className="animate-ping rounded-full border-4 border-t-4 border-raisin-black h-12 w-12"></div>
+        <div className="flex flex-col gap-12 h-screen items-center justify-center text-raisin-black">
+          <div className="flex flex-col items-center justify-center z-0">
+            <div className="absolute animate-ping rounded-full border-4 border-t-4 border-raisin-black h-12 w-12"></div>
+            <div className="absolute animate-ping delay-200 rounded-full border-4 border-t-4 border-golden-brown h-12 w-12"></div>
+          </div>
           <span className="text-2xl">Signing in... Please wait.</span>
         </div>
       </section>
