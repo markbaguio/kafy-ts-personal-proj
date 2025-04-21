5a51a3bf926a92847c4ac5daa4e81cfbb284511a
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-04-21 14:39:19 +0800

    WIP: fix positioning of loading animation.

--- src/components/ui/login-form.tsx
+++ src/components/ui/login-form.tsx
@@ -101,11 +101,12 @@ export function LoginForm({
     return (
       <section>
         <div className="flex flex-col gap-12 h-screen items-center justify-center text-raisin-black">
-          <div className="flex flex-col items-center justify-center z-0">
+          <div className="relative flex flex-col items-center justify-center z-0">
             <div className="absolute animate-ping rounded-full border-4 border-t-4 border-raisin-black h-12 w-12"></div>
             <div className="absolute animate-ping delay-200 rounded-full border-4 border-t-4 border-golden-brown h-12 w-12"></div>
+            <div className="absolute animate-ping delay-400 rounded-full border-4 border-t-4 border-royal-brown h-12 w-12"></div>
           </div>
-          <span className="text-2xl">Signing in... Please wait.</span>
+          <span className="text-2xl">Signing in...</span>
         </div>
       </section>
     );
