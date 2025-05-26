38e8cb5df25ff171c40ca5d1a46221131f94581f
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-05-26 11:55:20 +0800

    WIP: add test in unit test

--- src/pages/SignInPage/SignInPage.unit.test.tsx
+++ src/pages/SignInPage/SignInPage.unit.test.tsx
@@ -3,9 +3,19 @@ import userEvent from "@testing-library/user-event";
 import { SignInPage } from "./SignInPage";
 import { renderWithProviders } from "@/lib/test-utils";
 import { UserSignInSchema } from "@/schemas/auth/UserSignInFormSchema";
+import * as authServiceApi from "@/services/authServiceApi";
+import { SignInPayload } from "@/services/authServiceApi";
+import { ApiResponse } from "@/models/ApiResponse";
+import { Profile } from "@/models/types";
+import { successfulSignInProfileData } from "@/mocks/mockData/mockData";
+
+vi.mock("@/services/authServiceApi", () => ({
+  signInUser: vi.fn(),
+}));
+
+const user = userEvent.setup();
 
 describe("SignInPage", () => {
-  const user = userEvent.setup();
   it("renders the SignInPage component", () => {
     renderWithProviders(<SignInPage />, {}, "/auth/signin");
     expect(
@@ -34,5 +44,32 @@ describe("SignInPage", () => {
       password: "",
     });
     expect(result.success).toBe(false);
+  });
+
+  describe("Sign in user submit logic", () => {
+    it("calls signInUser with correct payload", async () => {
+      const mockPayload: SignInPayload = {
+        email: "test@example.com",
+        password: "securePassword123",
+      };
+
+      const mockResponse: ApiResponse<Profile> = {
+        statusCode: 200,
+        data: successfulSignInProfileData?.data,
+      };
+
+      const mockedSignInUser = vi.mocked(authServiceApi.signInUser);
+      mockedSignInUser.mockResolvedValueOnce(mockResponse);
+
+      const result: ApiResponse<Profile> = await authServiceApi.signInUser(
+        mockPayload
+      );
+
+      console.log(result);
+
+      expect(mockedSignInUser).toHaveBeenCalledTimes(1);
+      expect(mockedSignInUser).toHaveBeenCalledWith(mockPayload);
+      expect(result).toEqual(mockResponse);
+    });
   });
 });
