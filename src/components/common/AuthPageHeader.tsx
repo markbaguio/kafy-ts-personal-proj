b667831618c73c2017685bb5e1c3fd4334d56d32
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-05-28 15:15:46 +0800

    WIP: add data-testid on AuthPageHeader.

--- src/components/common/AuthPageHeader.tsx
+++ src/components/common/AuthPageHeader.tsx
@@ -3,7 +3,10 @@ import Logo from "./Logo";
 
 export default function AuthPageHeader() {
   return (
-    <header className="w-full bg-off-white shadow-md px-4 py-0">
+    <header
+      data-testid="auth-header"
+      className="w-full bg-off-white shadow-md px-4 py-0"
+    >
       <div className="container mx-auto flex items-center justify-between">
         <div className="flex items-center gap-8">
           {/** LOGO */}
