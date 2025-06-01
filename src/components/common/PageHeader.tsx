d573fa710ebe53d22004687cdae2cee557a7f6c7
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-06-01 15:14:47 +0800

    WIP: add data-testid to PageHeader.

--- src/components/common/PageHeader.tsx
+++ src/components/common/PageHeader.tsx
@@ -67,7 +67,10 @@ export default function PageHeader() {
 
   return (
     <>
-      <header className="w-full bg-off-white shadow-md px-4 py-0">
+      <header
+        date-testid="page-header"
+        className="w-full bg-off-white shadow-md px-4 py-0"
+      >
         <div className="container mx-auto flex items-center justify-between">
           <div className="flex items-center gap-8 md:gap-[70px]">
             {/** LOGO */}
