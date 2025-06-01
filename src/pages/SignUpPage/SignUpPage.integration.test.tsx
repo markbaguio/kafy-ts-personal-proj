2958915686e86c2f15bb85c0c196450a63f9c642
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-06-01 15:21:52 +0800

    WIP: add success flow test case in sign up page integration test.

--- src/pages/SignUpPage/SignUpPage.integration.test.tsx
+++ src/pages/SignUpPage/SignUpPage.integration.test.tsx
@@ -1,14 +1,16 @@
 import { AUTH_SIGN_UP, BASE_URL } from "@/constants";
 import { renderWithProviders } from "@/lib/test-utils";
 import { mockServer } from "@/mocks/server/server";
-import { QueryClient } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import userEvent from "@testing-library/user-event";
 import { http, HttpResponse } from "msw";
 import SignUpPage from "./SignUpPage";
 import { signUpPayload } from "@/services/authServiceApi";
-import { screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { UserSignUpRequestBody } from "@/mocks/handlers/handlers";
 import { ApiErrorResponse } from "@/models/ApiResponse";
+import { createMemoryRouter, RouterProvider } from "react-router";
+import { routes } from "@/routes/routes";
 
 const user = userEvent.setup();
 const queryClient = new QueryClient();
@@ -35,6 +37,71 @@ const mockSignUpData: mockSignUpDataType = {
 };
 
 describe("SignUpPage", () => {
+  describe("Success flow", () => {
+    it("redirects to the homepage and shows proper Header UI after successful sign-in", async () => {
+      const router = createMemoryRouter(routes, {
+        initialEntries: ["/auth/signup"],
+      });
+
+      render(
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      );
+
+      await user.type(
+        screen.getByTestId("first-name-input"),
+        mockSignUpData.firstName
+      );
+      await user.type(
+        screen.getByTestId("last-name-input"),
+        mockSignUpData.lastName
+      );
+      await user.type(screen.getByTestId("email-input"), mockSignUpData.email);
+      await user.type(
+        screen.getByTestId("password-input"),
+        mockSignUpData.password
+      );
+      await user.type(
+        screen.getByTestId("confirm-password-input"),
+        mockSignUpData.confirmPassword
+      );
+
+      await user.click(screen.getByTestId("signup-button"));
+      screen.debug(undefined, Infinity);
+
+      //? Assertions
+      //? Check if the UI is properly rendered on the homepage.
+      //? Pageheader
+      expect(await screen.findByTestId("page-header")).toBeInTheDocument();
+      expect(await screen.findByTestId("signout-button")).toBeInTheDocument();
+      expect(await screen.findByTestId("profile-button")).toBeInTheDocument();
+
+      //? Main content
+      expect(
+        await screen.findByText(
+          /More to sip and savor/i,
+          {},
+          { timeout: 2000 } //? this timeout is needed to wait for the [mockFetchSectionsData]  to fetch the data due to the mock delay of 1000ms using setTimeout.
+        )
+      );
+      expect(
+        await screen.findByText(
+          /Crafted for every cup/i,
+          {},
+          { timeout: 2000 } //? this timeout is needed to wait for the [mockFetchSectionsData]  to fetch the data due to the mock delay of 1000ms using setTimeout.
+        )
+      );
+
+      //? Foother
+      expect(
+        await screen.findByText(
+          "Fueled by ☕ & passion. Thanks for stopping by!"
+        )
+      );
+    });
+  });
+
   describe("Failure flow", () => {
     it("renders AuthErrorFallback when unexpected error occurs during sign-up", async () => {
       mockServer.use(
