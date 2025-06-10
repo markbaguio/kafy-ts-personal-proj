a346f6c0f56aa4028fb203fecb20a411709ca3fa
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-06-10 13:53:20 +0800

    FEAT: implement log out sync on all tabs using broadcast channel.

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,11 +2,13 @@ import Footer from "@/components/common/Footer";
 import PageHeader from "@/components/common/PageHeader/PageHeader";
 import { Outlet } from "react-router";
 import { useAuth } from "@/hooks/useAuth";
+import { useLogoutAuthAsync } from "@/hooks/useLogoutAuthSync";
 
 //TODO: implement broadcast channel to sync signout on all tabs.
 
 export default function MainLayout() {
   useAuth();
+  useLogoutAuthAsync();
 
   return (
     <div className="flex flex-col">
