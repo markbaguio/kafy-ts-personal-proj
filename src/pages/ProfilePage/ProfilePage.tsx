ec01ee6bd423ba76419a11bbc5718d4c5bbcc42e
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-06-20 17:21:32 +0800

    WIP: profile layout.

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -2,6 +2,7 @@ import { createGetProfileQueryOptions } from "@/queryOptions/createGetProfileQue
 import { useQuery } from "@tanstack/react-query";
 import { User } from "lucide-react";
 import ShoppingBagSVG from "@/assets/ProfilePage/shopping-bag-svgrepo-com.svg";
+import { Button } from "@/components/ui/button";
 
 export default function ProfilePage() {
   const { data } = useQuery(createGetProfileQueryOptions());
@@ -12,7 +13,6 @@ export default function ProfilePage() {
       <div className="overflow-hidden bg-milky-white rounded-[30px] shadow-xl row-span-2 col-span-2 lg:col-span-1 flex flex-col gap-10 items-center p-5">
         <div className="flex flex-col items-center gap-10 p-15 w-full">
           <div className="overflow-hidden  bg-royal-brown/50 rounded-full">
-            {/** img */}
             <User className="w-[200px] h-[200px]" />
           </div>
           <div className="flex flex-col text-center">
@@ -26,7 +26,28 @@ export default function ProfilePage() {
             </h6>
           </div>
         </div>
+        {/** Analytics 1 */}
         <div className="bg-off-white-2 border-1 w-full grid grid-rows-2 grid-cols-2 grid-flow-row gap-2 p-5 rounded-[15px]">
+          <div className="bg-milky-white rounded-[15px] flex flex-row gap-2 p-3">
+            {data?.data?.avatar_url === null ? (
+              <img
+                className="w-[60px] h-[60px]"
+                src={ShoppingBagSVG}
+                alt="Shopping bag"
+              />
+            ) : (
+              <img
+                className="w-[60px] h-[60px]"
+                src={data?.data?.avatar_url}
+                alt="Shopping bag"
+              />
+            )}
+
+            <div className="flex flex-col">
+              <h5 className="text-xl font-light">Order count</h5>
+              <p className="text-3xl">22</p>
+            </div>
+          </div>
           <div className="bg-milky-white rounded-[15px] flex flex-row gap-2 p-3">
             <img
               className="w-[60px] h-[60px]"
@@ -34,23 +55,37 @@ export default function ProfilePage() {
               alt="Shopping bag"
             />
             <div className="flex flex-col">
-              <h5 className="text-xl font-light">Order count</h5>
-              <p className="text-3xl">22</p>
+              <h5 className="text-xl font-light">Favorites</h5>
+              <p className="text-3xl">21</p>
             </div>
           </div>
-          <div className="bg-milky-white  rounded-[15px] flex flex-row gap-2 p-3">
+          <div className="bg-milky-white rounded-[15px] flex flex-row gap-2 p-3">
             <img
               className="w-[60px] h-[60px]"
               src={ShoppingBagSVG}
               alt="Shopping bag"
             />
             <div className="flex flex-col">
-              <h5 className="text-xl font-light">Favorites</h5>
-              <p className="text-3xl">210</p>
+              <h5 className="text-xl font-light">Kafy points</h5>
+              <p className="text-3xl">50</p>
             </div>
           </div>
-          <div className="bg-royal-brown rounded-[15px]">3</div>
-          <div className="bg-light-caramel rounded-[15px]">4</div>
+          <div className="bg-milky-white rounded-[15px] flex flex-row gap-2 p-3">
+            <img
+              className="w-[60px] h-[60px]"
+              src={ShoppingBagSVG}
+              alt="Shopping bag"
+            />
+            <div className="flex flex-col">
+              <h5 className="text-xl font-light">Top pick</h5>
+              <p className="text-3xl underline decoration-burnt-sienna">
+                Espresso
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="w-full flex justify-start">
+          <Button variant={"main"}>Edit profile</Button>
         </div>
       </div>
       <div className="bg-milky-white rounded-[30px] shadow-xl col-span-2">
