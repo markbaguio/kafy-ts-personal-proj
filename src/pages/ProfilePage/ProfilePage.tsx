ea48aea4977057d26455b52c81d1d653014e2f58
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-06-20 17:01:52 +0800

    WIP: add shopping bag svg.

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,6 +1,7 @@
 import { createGetProfileQueryOptions } from "@/queryOptions/createGetProfileQueryOptions";
 import { useQuery } from "@tanstack/react-query";
 import { User } from "lucide-react";
+import ShoppingBagSVG from "@/assets/ProfilePage/shopping-bag-svgrepo-com.svg";
 
 export default function ProfilePage() {
   const { data } = useQuery(createGetProfileQueryOptions());
@@ -8,14 +9,14 @@ export default function ProfilePage() {
   return (
     <section className="w-screen bg-off-white-2/50 grid grid-cols-1 lg:grid-flow-col lg:grid-cols-3 p-10 gap-15 min-h-[850px] max-h-lvh overflow-y-auto">
       {/** Profile grid */}
-      <div className="overflow-hidden bg-milky-white rounded-[30px] shadow-xl row-span-2 col-span-2 lg:col-span-1 flex flex-col gap-20">
-        <div className="bg-success-green flex flex-col items-center gap-10 py-10">
-          <div className="overflow-hidden w-[100px] h-[100px] bg-black-coffee rounded-full">
+      <div className="overflow-hidden bg-milky-white rounded-[30px] shadow-xl row-span-2 col-span-2 lg:col-span-1 flex flex-col gap-10 items-center p-5">
+        <div className="flex flex-col items-center gap-10 p-15 w-full">
+          <div className="overflow-hidden  bg-royal-brown/50 rounded-full">
             {/** img */}
-            <User className="w-[100px] h-[100px]" />
+            <User className="w-[200px] h-[200px]" />
           </div>
           <div className="flex flex-col text-center">
-            <h1 className="text-3xl font-bold">
+            <h1 className="text-4xl font-bold">
               {data?.data?.first_name} {""}
               {data?.data?.last_name}
             </h1>
@@ -25,7 +26,32 @@ export default function ProfilePage() {
             </h6>
           </div>
         </div>
-        <div className="bg-burnt-sienna w-full">basic analytics</div>
+        <div className="bg-off-white-2 border-1 w-full grid grid-rows-2 grid-cols-2 grid-flow-row gap-2 p-5 rounded-[15px]">
+          <div className="bg-milky-white rounded-[15px] flex flex-row gap-2 p-3">
+            <img
+              className="w-[60px] h-[60px]"
+              src={ShoppingBagSVG}
+              alt="Shopping bag"
+            />
+            <div className="flex flex-col">
+              <h5 className="text-xl font-light">Order count</h5>
+              <p className="text-3xl">22</p>
+            </div>
+          </div>
+          <div className="bg-milky-white  rounded-[15px] flex flex-row gap-2 p-3">
+            <img
+              className="w-[60px] h-[60px]"
+              src={ShoppingBagSVG}
+              alt="Shopping bag"
+            />
+            <div className="flex flex-col">
+              <h5 className="text-xl font-light">Favorites</h5>
+              <p className="text-3xl">210</p>
+            </div>
+          </div>
+          <div className="bg-royal-brown rounded-[15px]">3</div>
+          <div className="bg-light-caramel rounded-[15px]">4</div>
+        </div>
       </div>
       <div className="bg-milky-white rounded-[30px] shadow-xl col-span-2">
         Analytics
