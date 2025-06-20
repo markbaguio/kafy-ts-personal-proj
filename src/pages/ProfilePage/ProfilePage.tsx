6ae2bdfb6dcee6b60109880285d76d7ce5e69e48
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-06-20 15:35:57 +0800

    WIP: profile layout.

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,19 +1,21 @@
 import { createGetProfileQueryOptions } from "@/queryOptions/createGetProfileQueryOptions";
 import { useQuery } from "@tanstack/react-query";
+import { User } from "lucide-react";
 
 export default function ProfilePage() {
   const { data } = useQuery(createGetProfileQueryOptions());
 
   return (
-    <main className="w-screen bg-off-white-2/50 grid grid-cols-1 lg:grid-flow-col lg:grid-cols-3 p-10 gap-15 min-h-[850px] max-h-lvh overflow-y-auto">
+    <section className="w-screen bg-off-white-2/50 grid grid-cols-1 lg:grid-flow-col lg:grid-cols-3 p-10 gap-15 min-h-[850px] max-h-lvh overflow-y-auto">
       {/** Profile grid */}
-      <div className="bg-milky-white rounded-[30px] shadow-lg row-span-2 col-span-2 lg:col-span-1 ">
-        <div className="flex flex-col items-center gap-10">
-          <div className="w-[100px] h-[100px] bg-black-coffee rounded-full">
+      <div className="overflow-hidden bg-milky-white rounded-[30px] shadow-xl row-span-2 col-span-2 lg:col-span-1 flex flex-col gap-20">
+        <div className="bg-success-green flex flex-col items-center gap-10 py-10">
+          <div className="overflow-hidden w-[100px] h-[100px] bg-black-coffee rounded-full">
             {/** img */}
+            <User className="w-[100px] h-[100px]" />
           </div>
-          <div className="flex flex-col text-center gap-1">
-            <h1 className="text-3xl">
+          <div className="flex flex-col text-center">
+            <h1 className="text-3xl font-bold">
               {data?.data?.first_name} {""}
               {data?.data?.last_name}
             </h1>
@@ -23,13 +25,14 @@ export default function ProfilePage() {
             </h6>
           </div>
         </div>
+        <div className="bg-burnt-sienna w-full">basic analytics</div>
       </div>
-      <div className="bg-milky-white rounded-[30px] shadow-lg col-span-2">
+      <div className="bg-milky-white rounded-[30px] shadow-xl col-span-2">
         Analytics
       </div>
-      <div className="bg-milky-white rounded-[30px] shadow-lg col-span-2">
+      <div className="bg-milky-white rounded-[30px] shadow-xl col-span-2">
         Order history
       </div>
-    </main>
+    </section>
   );
 }
