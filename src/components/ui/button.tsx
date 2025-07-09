b962ab5162d172725c624ed876e26a600ce785cf
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-07-09 16:19:11 +0800

    wip: menu page layout.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost:
           "font-semibold hover:text-golden-brown hover:underline hover:cursor-pointer",
+        ghost2: "font-semibold hover:text-golden-brown hover:cursor-pointer",
         link: "text-primary underline-offset-4 hover:underline",
         main: "text-off-white bg-raisin-black rounded-full hover:opacity-80",
         icon: "hover:opacity-70 bg-raisin-black rounded-full text-milky-white w-[100px]",
--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,21 +1,53 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
 
 export function Menu() {
   return (
-    // <main className="w-full bg-off-white-2">
-    //   <div className="w-full p-5">
-    //     <h1 className="text-5xl">Menu</h1>
-    //   </div>
-    //   <Tabs defaultValue="hot" orientation="vertical" className="w-full">
-    //     <TabsList className="w-1/4">
-    //       <TabsTrigger value="hot">hot</TabsTrigger>
-    //       <TabsTrigger value="cold">cold</TabsTrigger>
-    //     </TabsList>
-    //     <TabsContent value="hot">
-    //       Make changes to your account here.
-    //     </TabsContent>
-    //     <TabsContent value="cold">Change your password here.</TabsContent>
-    //   </Tabs>
-    // </main>
+    <div className="w-full">
+      {/** Ribbon/banner */}
+      <div className="w-full bg-light-caramel/30 p-5 flex justify-start">
+        <Button className="text-lg font-normal" variant="ghost2" asChild>
+          <Link to="/favorites">
+            <span>Favorites</span>
+          </Link>
+        </Button>
+        <Button className="text-lg font-normal" variant="ghost2" asChild>
+          <Link to="/favorites">
+            <span>Features</span>
+          </Link>
+        </Button>
+        <Button className="text-lg font-normal" variant="ghost2" asChild>
+          <Link to="/favorites">
+            <span>Previous</span>
+          </Link>
+        </Button>
+      </div>
+      {/** Main */}
+      <div className="flex w-full">
+        <MenuSidebar />
+        <main className="w-full h-lvh p-5 bg-success-green">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente,
+          eligendi.
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export function MenuSidebar() {
+  return (
+    <aside className="h-lvh w-[300px] p-5">
+      <div>
+        <span className="text-2xl font-bold">Category</span>
+        <Button
+          className="w-full text-2xl font-light"
+          variant="ghost2"
+          size="lg"
+          asChild
+        >
+          <Link to="/hot">Hot</Link>
+        </Button>
+      </div>
+    </aside>
   );
 }
