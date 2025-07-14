07a330901509ee100f992e5a3312c0f584544f54
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-07-14 15:57:59 +0800

    wip: add menu item card.

--- src/pages/MenuPage/MenuPage.tsx
+++ src/pages/MenuPage/MenuPage.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button";
+import { MenuItemsMockData } from "@/constants";
 import { cn } from "@/lib/utils";
+import { MenuItemType } from "@/schemas/Menu/MenuItemSchema";
 import { MenuIcon } from "lucide-react";
 import { useState } from "react";
 import { Link } from "react-router";
@@ -43,10 +45,13 @@ export function MenuPage() {
             isOpen ? "flex" : "hidden"
           }`}
         />
-        {/* <MenuSidebar className="h-lvh w-[300px] flex flex-col gap-10 py-15" /> */}
-        <main className="w-full h-lvh p-5 py-15 bg-success-green">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente,
-          eligendi.
+        <main className="w-full h-full p-5 py-15 flex flex-wrap gap-5">
+          <div className="w-full h-fit">
+            <h3 className="text-2xl font-bold">Menu</h3>
+          </div>
+          {MenuItemsMockData.map((menuItem) => (
+            <MenuItemCard key={menuItem.id} {...menuItem} />
+          ))}
         </main>
       </div>
     </div>
@@ -59,7 +64,7 @@ export function MenuSidebar({ className }: React.ComponentProps<"aside">) {
     <aside className={cn(className)}>
       <div className="w-full flex flex-col justify-center  gap-4">
         <div className="">
-          <span className="text-2xl font-bold">Drinks</span>
+          <h3 className="text-2xl font-bold">Drinks</h3>
         </div>
         <div
           className="flex flex-col gap-1
@@ -87,7 +92,7 @@ export function MenuSidebar({ className }: React.ComponentProps<"aside">) {
       </div>
       <div className="w-full flex flex-col justify-center  gap-4">
         <div className="">
-          <span className="text-2xl font-bold">Food</span>
+          <h3 className="text-2xl font-bold">Food</h3>
         </div>
         <div
           className="text-2xl font-light flex flex-col gap-1
@@ -130,5 +135,34 @@ export function MenuSidebar({ className }: React.ComponentProps<"aside">) {
         </div>
       </div>
     </aside>
+  );
+}
+
+export type MenuItemProps = MenuItemType;
+
+export function MenuItemCard({
+  id,
+  category,
+  description,
+  image_url,
+  is_available,
+  name,
+  price,
+}: MenuItemProps) {
+  return (
+    <div
+      className={cn(
+        "bg-milky-white text-raisin-black flex flex-col gap-6 rounded-xl border p-6 py-6 shadow-lg min-w-[300px] max-w-[300px] min-h-[300px]"
+      )}
+    >
+      <img className="rounded-sm" src={image_url} alt={`${image_url}`} />
+      <div>
+        <h2 className="text-xl font-normal">{name}</h2>
+        <p className=" text-sm font-light text-raisin-black-muted">
+          {description}
+        </p>
+        <span className="text-success-green text-lg font-light">{price}</span>
+      </div>
+    </div>
   );
 }
