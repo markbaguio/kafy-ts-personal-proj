03bfd781f9b25d388fbce0d24180c7f2af8ef281
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-07-17 10:53:02 +0800

    chore: update import path of the menu page in routes.tsx.

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,12 +3,12 @@ import MainLayout from "@/layouts/MainLayout";
 import AboutUs from "@/pages/AboutUs";
 import GiftCards from "@/pages/GiftCards";
 import Homepage from "@/pages/Homepage";
+import MenuPage from "@/pages/MenuPage/MenuPage";
 import NotFoundPage from "@/pages/NotFoundPage";
 import ProfilePage from "@/pages/ProfilePage/ProfilePage";
 import Rewards from "@/pages/Rewards";
 import { SignInPage } from "@/pages/SignInPage/SignInPage";
 import SignUpPage from "@/pages/SignUpPage/SignUpPage";
-import { MenuPage } from "@/pages/MenuPage/MenuPage";
 import { RouteObject } from "react-router";
 
 export const routes: RouteObject[] = [
