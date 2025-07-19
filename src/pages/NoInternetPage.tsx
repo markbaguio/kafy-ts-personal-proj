fe46b77cb3814a5407787e092f44870a81d34512
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-07-19 17:54:59 +0800

    wip(NoInternetPage.tsx): add responsive classes.

--- src/pages/NoInternetPage.tsx
+++ src/pages/NoInternetPage.tsx
@@ -7,12 +7,16 @@ export function NoInternetPage() {
 
   return (
     <div className="flex flex-col gap-5 items-center justify-center h-screen bg-milky-white">
-      <img src={noInternetSvg} className="w-1/2 h-1/2" alt="No internet svg." />
+      <img
+        src={noInternetSvg}
+        className="w-full lg:w-1/2 h-1/2"
+        alt="No internet svg."
+      />
       <div className="flex flex-col h-fit items-center gap-0 p-0">
-        <h1 className="text-4xl/4 font-bold text-red-600 mb-4">
+        <h1 className="text-lg/4 md:text-xl lg:text-4xl/4 font-bold text-center mb-4">
           No Internet Connection
         </h1>
-        <p className="text-lg/4 text-gray-700 mb-6">
+        <p className="text-sm/4 lg:text-lg/4 text-center  mb-6">
           Please check your internet connection and try again.
         </p>
       </div>
