ab8e76e8806d8549463bfdd927e3e6ffc63ab6f7
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-07-19 17:48:54 +0800

    wip(NoInternetPage.tsx): add NoInternet page.

---
+++ src/pages/NoInternetPage.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router";
+import noInternetSvg from "@/assets/NoInternetPage/undraw_server-down_lxs9.svg"; // Adjust the path as necessary
+
+export function NoInternetPage() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col gap-5 items-center justify-center h-screen bg-milky-white">
+      <img src={noInternetSvg} className="w-1/2 h-1/2" alt="No internet svg." />
+      <div className="flex flex-col h-fit items-center gap-0 p-0">
+        <h1 className="text-4xl/4 font-bold text-red-600 mb-4">
+          No Internet Connection
+        </h1>
+        <p className="text-lg/4 text-gray-700 mb-6">
+          Please check your internet connection and try again.
+        </p>
+      </div>
+      <div className="flex gap-3">
+        <Button variant="main" onClick={() => window.location.reload()}>
+          Retry
+        </Button>
+        <Button variant="outline2" onClick={() => navigate("/")}>
+          Home
+        </Button>
+      </div>
+    </div>
+  );
+}
