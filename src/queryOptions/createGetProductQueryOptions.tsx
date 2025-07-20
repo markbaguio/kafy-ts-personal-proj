e27f07d9e3e806e8346637cfe69ac6c77f70768a
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-07-20 16:17:56 +0800

    fix: change data return type from Products[] to PaginatedProducts.

---
+++ src/queryOptions/createGetProductQueryOptions.tsx
@@ -0,0 +1,24 @@
+import { ApiResponse } from "@/models/ApiResponse";
+import { PaginatedProducts, Product } from "@/models/types";
+import { getAllProducts } from "@/services/productService";
+import { queryOptions, UseQueryOptions } from "@tanstack/react-query";
+
+type GetProductQueryOptionsParam = {
+  page: string;
+  catergory: string;
+};
+
+export function createGetProductQueryOptions(
+  params?: GetProductQueryOptionsParam,
+  options?: Omit<
+    UseQueryOptions<ApiResponse<PaginatedProducts>>,
+    "queryKey" | "queryFn"
+  >
+) {
+  return queryOptions({
+    ...options,
+    queryKey: ["products", params],
+    queryFn: () =>
+      getAllProducts({ page: params?.page, category: params?.catergory }),
+  });
+}
