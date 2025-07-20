8f0027253e9c507a3db10142e09a7d64038a7fc1
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-07-20 14:35:48 +0800

    wip(MainLayout.tsx): add condition to check for internet connection.

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,12 +3,20 @@ import PageHeader from "@/components/common/PageHeader/PageHeader";
 import { Outlet } from "react-router";
 import { useAuth } from "@/hooks/useAuth";
 import { useLogoutAuthAsync } from "@/hooks/useLogoutAuthSync";
+import { useOnlineStatus } from "@/hooks/useOnlineStatus";
+import { NoInternetPage } from "@/pages/NoInternetPage";
 
 //TODO: implement broadcast channel to sync signout on all tabs.
 
 export default function MainLayout() {
   useAuth();
   useLogoutAuthAsync();
+
+  const isOnline = useOnlineStatus();
+
+  if (!isOnline) {
+    return <NoInternetPage />;
+  }
 
   return (
     <div className="flex flex-col">
