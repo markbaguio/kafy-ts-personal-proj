4f91b7ec99848f6506ef13271c0486ffa269f090
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-07-22 17:05:45 +0800

    fix: change the front end route for brevity.

--- src/components/common/PageHeader/PageHeader.tsx
+++ src/components/common/PageHeader/PageHeader.tsx
@@ -26,7 +26,7 @@ type navItemType = {
 
 const navItems: navItemType[] = [
   { name: "HOME", path: "/" },
-  { name: "MENU", path: "/api/menu" },
+  { name: "MENU", path: "/menu" },
   { name: "REWARDS", path: "/rewards" },
   { name: "GIFT CARDS", path: "/gift" },
 ];
