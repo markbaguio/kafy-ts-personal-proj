e277b94090ac3c010c92d014fa6060208d5165dc
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-07-25 15:02:46 +0800

    wip(useMenuDetailPageSearchParams.ts): add dedicated search param hook for MenuDetailPage.tsx

---
+++ src/hooks/useMenuPageDetailSearchParams.ts
@@ -0,0 +1,59 @@
+import { useSearchParams } from "react-router";
+import { z } from "zod";
+
+const ProductSizeSchema = z.enum(["S", "M", "L"]).default("S");
+
+const MenuPageDetailSchema = z.object({
+  size: ProductSizeSchema,
+  quantity: z
+    .string()
+    .regex(/^\d+(\.\d+)?$/)
+    .default("1"),
+});
+
+export default function useMenuDetailPageSearchParams() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const rawMenudDetailPageSearchParams = {
+    size: searchParams.get(`size`) ?? "S",
+    quantity: searchParams.get("quantity"),
+  };
+
+  const parsedrMenudDetailPageSearchParams = MenuPageDetailSchema.safeParse(
+    rawMenudDetailPageSearchParams
+  );
+
+  if (!parsedrMenudDetailPageSearchParams.success) {
+    //? if the parse fails. Return default values.
+    return {
+      quantity: "1",
+      size: "S",
+      setSearchParams,
+    };
+  }
+
+  return {
+    quantity: parsedrMenudDetailPageSearchParams.data.quantity,
+    size: parsedrMenudDetailPageSearchParams.data.size,
+    setSearchParams,
+  };
+}
+
+// export default function useMenuPageDetaileSearchParams(
+//   ...inputParams: string[]
+// ): Record<string, string | null> {
+//   //   const result: Record<string, string | null> = {};
+//   const defaultMenuPageDetailSearchParams: Record<string, string | null> = {};
+
+//   inputParams.forEach((param) => {
+//     defaultMenuPageDetailSearchParams[param] = param;
+//   });
+
+//   const [searchParams, setSearchParams] = useSearchParams(defaultMenuPageDetailSearchParams);
+
+//   //   inputParams.forEach((inputParam) => {
+//   //     result[inputParam] = inputParam;
+//   //   });
+
+//   return result;
+// }
