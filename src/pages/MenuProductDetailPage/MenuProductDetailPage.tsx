2e60653f872c90601f11a9d3e648ed672b1a3ac1
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-07-25 15:28:16 +0800

    fix(MenProductDetailPage.tsx): replace mock data with real data.

--- src/pages/MenuProductDetailPage/MenuProductDetailPage.tsx
+++ src/pages/MenuProductDetailPage/MenuProductDetailPage.tsx
@@ -4,19 +4,18 @@ import {
   ProductSize,
   ProductSizeOptions,
 } from "@/schemas/MenuProductDetailPage/MenuProductDetailParamsSchema";
-import { useParams, useSearchParams } from "react-router";
+import { useParams } from "react-router";
 import { PageNotFound } from "../PageNotFound";
 import imageSkeleton from "@/assets/rewards/image-skeleton.svg";
-import { useQuery } from "@tanstack/react-query";
-import createProductDetailQueryOptions from "@/queryOptions/createGetProductDetailQueryOptions";
 import { Minus, Plus, Star } from "lucide-react";
 import { PESOSIGN } from "@/constants";
 import { Button } from "@/components/ui/button";
-import { Separator } from "@radix-ui/react-separator";
 import { isValidProductSize } from "@/lib/utils";
+import useMenuDetailPageSearchParams from "@/hooks/useMenuPageDetailSearchParams";
+import { useQuery } from "@tanstack/react-query";
+import createProductDetailQueryOptions from "@/queryOptions/createGetProductDetailQueryOptions";
 
 //TODO: Fix quantityStepper change in width when then quantity > 1
-//TODO: Sanitize search Params before adding to cart.
 
 export default function MenuProductDetailPage() {
   const params = useParams<MenuProductDetailPageParams>();
@@ -27,35 +26,37 @@ export default function MenuProductDetailPage() {
     return <PageNotFound />;
   }
 
-  const [searchParams, setSearchParams] = useSearchParams({
-    size: "S",
-    quantity: "1",
-  });
+  const {
+    quantity: quantityParam,
+    size: sizeParam,
+    setSearchParams,
+  } = useMenuDetailPageSearchParams();
+
+  console.log(`quantity: ${quantityParam} size: ${sizeParam}`);
 
   //? Derived/Computed value
-  const rawSize: string = searchParams.get("size") ?? "S";
+  const rawSize: string = sizeParam;
   const size: ProductSize = isValidProductSize(rawSize) ? rawSize : "S";
-  const quantity = Math.max(parseInt(searchParams.get("quantity") ?? "1"), 1);
+  const quantity = Math.max(parseInt(quantityParam ?? "1"), 1);
   const parsedQuantity = isNaN(quantity) ? 1 : quantity;
 
   function handleSizeChange(size: ProductSize) {
-    searchParams.set("size", size);
-    setSearchParams(searchParams);
+    setSearchParams({ size: size, quantity: quantityParam });
   }
 
   function handleQuantityChange(quantity: number) {
-    const sanitizedQuantity = Math.max(1, quantity);
-    searchParams.set("quantity", sanitizedQuantity.toString());
-    setSearchParams(searchParams);
+    const sanitizedQuantity = Math.max(1, quantity).toString();
+    setSearchParams({ size: sizeParam, quantity: sanitizedQuantity });
   }
 
-  // const { data } = useQuery(
-  //   createProductDetailQueryOptions({
-  //     product_id: parsedParams.data.product_id,
-  //   })
-  // );
+  const { data } = useQuery(
+    createProductDetailQueryOptions({
+      product_id: parsedParams.data.product_id,
+    })
+  );
 
   // console.log("ProductDetail:", data);
+
   function handleAddToCart({
     size,
     quantity,
@@ -68,49 +69,45 @@ export default function MenuProductDetailPage() {
     console.log(`quantity: ${parsedQuantity}`);
   }
 
-  const MockProductDetail = {
-    id: 10,
-    category: "hot",
-    created_at: "2025-07-01T05:31:56.115117+00:00",
-    description:
-      "A flavorful chai tea latte. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    image_url:
-      "https://images.unsplash.com/photo-1578899952107-9c390f1af1b7?w=900",
-    // null,
-    is_available: false,
-    name: "Chai Latte",
-    price: 150,
-    updated_at: null,
-  };
+  // const MockProductDetail = {
+  //   id: 10,
+  //   category: "hot",
+  //   created_at: "2025-07-01T05:31:56.115117+00:00",
+  //   description:
+  //     "A flavorful chai tea latte. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  //   image_url:
+  //     "https://images.unsplash.com/photo-1578899952107-9c390f1af1b7?w=900",
+  //   // null,
+  //   is_available: false,
+  //   name: "Chai Latte",
+  //   price: 150,
+  //   updated_at: null,
+  // };
 
   return (
-    <div className="w-full min-h-full h-full flex flex-col gap-2 px-20 py-5 bg-off-white-2">
+    <div className="w-full min-h-full h-full flex flex-col gap-2 px-20 py-5 bg-off-white-2/50">
       <div className="flex-none h-fit bg-burnt-sienna ">Breadcrumbs</div>
       <main className="grow-1 flex flex-col bg-milky-white rounded-xl shadow-lg">
-        {/** Produc Image and  settings.*/}
+        {/** Product Image and  settings.*/}
         <div className="flex w-full flex-row p-10 gap-10">
           <div className="rounded-lg overflow-hidden">
             <img
-              src={MockProductDetail.image_url ?? imageSkeleton}
+              src={data?.data?.image_url ?? imageSkeleton}
               alt="Product img"
-              // width={650}
-              // height={650}
               className="w-[600px] h-[700px] object-cover"
             />
           </div>
           <div className="w-1/2 p-10 flex flex-col gap-5">
             <div>
-              <h1 className="text-5xl/tight font-bold">
-                {MockProductDetail.name}
-              </h1>
+              <h1 className="text-5xl/tight font-bold">{data?.data?.name}</h1>
               <MockRatingComponent rating={4} />
             </div>
             <span className="text-4xl font-bold">
               {PESOSIGN}
-              {MockProductDetail.price}
+              {data?.data?.price}
             </span>
             <p className="font-light text-[16px] text-justify">
-              {MockProductDetail.description}
+              {data?.data?.description}
             </p>
             <ProductOptions
               size={size}
@@ -132,6 +129,7 @@ export default function MenuProductDetailPage() {
           </div>
         </div>
       </main>
+      {/** Mock Nutritional values */}
       <div className="grow-0 flex-auto bg-burnt-sienna">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
         commodi neque nostrum nobis quisquam eum aperiam deserunt ratione,
