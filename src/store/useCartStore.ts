c56a76c0aadf3237bae84fdc3f7ae66c369b07fd
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-07-27 15:51:05 +0800

    wip(useCartStore.ts): add zustand cart store.

---
+++ src/store/useCartStore.ts
@@ -0,0 +1,100 @@
+import { CartProduct, OrderItem } from "@/models/types";
+import { create, createStore } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface CartState {
+  cartProducts: CartProduct[];
+}
+
+interface CartActions {
+  addProductToCart: (cartProduct: CartProduct) => void;
+  removeProductFromCart: (productID: number) => void;
+  clearCart: () => void;
+}
+
+export type CartStore = CartState & CartActions;
+
+export const useCartStore = create<CartStore>()(
+  persist(
+    (set) => ({
+      cartProducts: [],
+
+      addProductToCart: (cartProduct) =>
+        set((state) => {
+          // cartProducts: [...state.cartProducts, cartProduct],
+          //? handle existing product in the cart.
+          //? if a product is already in the cart that product will be incremented/combined.
+
+          //? if the product to be added is already in the cart.
+          //? the cartProduct in this case. The quantity of the existing product in the cart
+          //? will be added with the cartProduct.quantity.
+
+          //? check if the product with the same size and product_id exist in the cart.
+          const existingIndex = state.cartProducts.findIndex(
+            (product) =>
+              product.product_id === cartProduct.product_id &&
+              product.product_size === cartProduct.product_size
+          );
+
+          //? if the product is already existing. Increment it's quantity.
+          //? .findByIndex returns -1 if no elements satisfy the condition.
+          if (existingIndex !== -1) {
+            //? create a copy of the current cartProducts.
+            //? update the copy of the current cartProducts.
+            const updatedCartProducts = [...state.cartProducts];
+
+            //? get the existingProduct
+            const existingProduct = updatedCartProducts[existingIndex];
+
+            //? update the quantity of the existing product in the cart.
+            updatedCartProducts[existingIndex] = {
+              ...existingProduct,
+              quantity: existingProduct.quantity + cartProduct.quantity,
+            };
+          }
+
+          //? if the product to be added is unique, simply append it to the current
+          //? cartProducts, in the state.cartProducts
+          return {
+            cartProducts: [...state.cartProducts, cartProduct],
+          };
+        }),
+      // addProductToCart: (cartProduct) =>
+      //   set((state) => {
+      //     const existingIndex = state.cartProducts.findIndex(
+      //       (product) =>
+      //         product.product_id === cartProduct.product_id &&
+      //         product.product_size === cartProduct.product_size
+      //     );
+
+      //     // If product already exists, increase quantity
+      //     if (existingIndex !== -1) {
+      //       const updatedProducts = [...state.cartProducts];
+      //       const existingProduct = updatedProducts[existingIndex];
+
+      //       updatedProducts[existingIndex] = {
+      //         ...existingProduct,
+      //         quantity: existingProduct.quantity + cartProduct.quantity,
+      //       };
+
+      //       return { cartProducts: updatedProducts };
+      //     }
+
+      //     // Otherwise, add as new
+      //     return {
+      //       cartProducts: [...state.cartProducts, cartProduct],
+      //     };
+      //   }),
+
+      removeProductFromCart: (productID) =>
+        set((state) => ({
+          cartProducts: state.cartProducts.filter(
+            (product) => product.product_id !== productID
+          ),
+        })),
+
+      clearCart: () => set({ cartProducts: [] }),
+    }),
+    { name: "cart" }
+  )
+);
