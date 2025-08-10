3e6840137e80882dff3306f7e845fac1f15cb0a2
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-08-10 15:49:46 +0800

    wip(placeOrderService.ts): add place order fn for mutationFn.

---
+++ src/services/placeOrderService.ts
@@ -0,0 +1,7 @@
+import { PlaceOrderPayloadType } from "@/models/types";
+
+export async function placeOrder(payload: PlaceOrderPayloadType) {
+  console.log("placeOrder: ", payload);
+
+  return;
+}
