4550dcbd69e99fa145138e44c4058878cca24281
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-08-13 16:05:53 +0800

    wip(responseInterceptor.ts): add baseline for 401/403 error response interceptor for refreshing access token.

--- src/lib/axiosInterceptors/responseInterceptor.ts
+++ src/lib/axiosInterceptors/responseInterceptor.ts
@@ -1,17 +1,28 @@
 import { refreshAccessToken } from "@/services/authServiceApi";
-import axios, { AxiosError, isAxiosError } from "axios";
+import axios, { AxiosError, AxiosRequestConfig, isAxiosError } from "axios";
 
 //! RefreshAccessToken doesn't work it's throwing AuthApiError: Invalid Refresh Token: Already Used
 
+// interface AxiosRequestConfigWithRetry extends AxiosRequestConfig {
+//   _alreadyRefreshedOnce: boolean;
+// }
+
 export const axiosInstance = axios.create();
 
+//TODO Try this. Use this as baseline if this works then improve.
 axiosInstance.interceptors.response.use(
   (response) => {
     return response;
   },
   async (error: AxiosError) => {
     const originalRequest = error.config;
+
+    // let _alreadyRefreshedOnce: boolean = originalRequest._alreadyRefreshedOnce;
     if (error.status === 403 || error.status === 401) {
+      // console.log("1: ", _alreadyRefreshedOnce);
+      // _alreadyRefreshedOnce = true;
+      // console.log("2: ", _alreadyRefreshedOnce);
+
       try {
         await refreshAccessToken();
         if (originalRequest) {
@@ -22,7 +33,7 @@ axiosInstance.interceptors.response.use(
           console.error(error);
         }
       }
-      return Promise.reject(error);
     }
+    return Promise.reject(error);
   }
 );
