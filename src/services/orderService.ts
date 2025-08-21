ee4f9594002195f2e9b8e0b857ffcb4ea5ab54c1
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-08-21 14:18:30 +0800

    wip(orderService.ts): add order service that handles get, post... methods for orders.

---
+++ src/services/orderService.ts
@@ -0,0 +1,63 @@
+import { AxiosErrorCode, BASE_URL, ORDERS_PAGE } from "@/constants";
+import { axiosInstance } from "@/lib/axiosInterceptors/responseInterceptor";
+import { isApiErrorResponse } from "@/lib/utils";
+import { ApiErrorResponse, ApiResponse } from "@/models/ApiResponse";
+import { OrdersWithOrderItemsWithImage } from "@/models/types";
+import { OrdersWithOrderItemsWithImageSchemaArray } from "@/schemas/OrderSchema/Order/OrderSchema";
+import { isAxiosError } from "axios";
+import { ZodError } from "zod";
+
+type GetOrdersParams = {
+  status?: string;
+};
+
+export async function getOrders({
+  status = "orderPlaced",
+}: GetOrdersParams): Promise<ApiResponse<OrdersWithOrderItemsWithImage>> {
+  //? This Promise timeout simulates network latency to show edge case (loading state)
+  //TODO: remove this delay. this might be the cause of the refresh auth token not working properly.
+  //   await new Promise((resolve) => setTimeout(resolve, 1500));
+  try {
+    console.log("Fetching orders with status:", status);
+    const response = await axiosInstance.get<
+      ApiResponse<OrdersWithOrderItemsWithImage>
+    >(`${BASE_URL}${ORDERS_PAGE}`, {
+      params: { status },
+      withCredentials: true,
+    });
+
+    const parsedOrdersWithOrderItemsWithImage =
+      OrdersWithOrderItemsWithImageSchemaArray.safeParse(response.data.data);
+
+    if (!parsedOrdersWithOrderItemsWithImage.success) {
+      throw new ZodError(parsedOrdersWithOrderItemsWithImage.error.errors);
+    }
+
+    console.log("response", response);
+
+    return {
+      statusCode: response.status,
+      data: parsedOrdersWithOrderItemsWithImage.data,
+    };
+  } catch (error) {
+    if (isAxiosError(error)) {
+      const responseErrorData: ApiErrorResponse = error.response?.data; //? Check if there are specific error response.
+      if (error.code === AxiosErrorCode.NetworkError) {
+        throw new ApiErrorResponse(
+          503,
+          "ERR_NETWORK",
+          "Unable to reach server. Please check your internet connection."
+        );
+      }
+      if (responseErrorData && isApiErrorResponse(responseErrorData)) {
+        throw new ApiErrorResponse(
+          responseErrorData.statusCode,
+          responseErrorData.errorName,
+          responseErrorData.message,
+          responseErrorData.errorDetails
+        );
+      }
+    }
+    throw new Error("An unexpected error occurred");
+  }
+}
