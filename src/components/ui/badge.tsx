94cf61584683a499295c50594cdf32386d2e92a3
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-08-31 16:15:27 +0800

    wip(badge.tsx): add additional variants that reflect orderStatus.

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -20,7 +20,13 @@ const badgeVariants = cva(
         secondaryBestSeller:
           "border-none bg-black-coffee/10 text-phthalo [a&]:hover:bg-secondary/90",
         orderInProgress:
-          "border-none bg-success-green/20 rounded-full px-3 py-2",
+          "border-none bg-success-green/20 rounded-full px-3 py-2 text-success-green-accent",
+        orderPlaced:
+          "border-none bg-success-green/20 rounded-full px-3 py-2 text-success-green-accent",
+        completed:
+          "border-none bg-success-green/20 rounded-full px-3 py-2 text-success-green-accent",
+        canceled:
+          "boreder-none bg-destructive/20 rounded-full px-3 py-2 text-destructive/50",
       },
     },
     defaultVariants: {
