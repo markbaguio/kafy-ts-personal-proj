26e110fd4a08127970411c62f50eceb194f2bfda
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-09-16 16:10:28 +0800

    fix(createGetUserFavoriteProductsQueryOptions.ts): change options type to UseQueryOptions.

--- src/queryOptions/createGetUserFavoriteProductsQueryOptions.ts
+++ src/queryOptions/createGetUserFavoriteProductsQueryOptions.ts
@@ -1,11 +1,11 @@
 import { ApiResponse } from "@/models/ApiResponse";
 import { UserFavoriteProducts } from "@/models/types";
 import { getUserFavoriteProducts } from "@/services/productService";
-import { queryOptions, QueryOptions } from "@tanstack/react-query";
+import { queryOptions, UseQueryOptions } from "@tanstack/react-query";
 
 export function createGetUserFavoriteProductsQueryOptions(
   options?: Omit<
-    QueryOptions<ApiResponse<UserFavoriteProducts>>,
+    UseQueryOptions<ApiResponse<UserFavoriteProducts>>,
     "queryKey" | "queryFn"
   >
 ) {
