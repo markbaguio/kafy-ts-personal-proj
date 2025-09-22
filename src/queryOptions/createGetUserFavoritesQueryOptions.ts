a59da0cc7c1b2219b69bd800753b0d9f1b8ef235
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-09-22 14:39:45 +0800

    fix(createGetUserFavoritesQueryOptions.ts): change query data from ApiResponse<UserFavoriteProducts> to UserFavoriteProduct. This ensures that the data saved in the useQuery cache is of type UserFavoriteProducts.

--- src/queryOptions/createGetUserFavoritesQueryOptions.ts
+++ src/queryOptions/createGetUserFavoritesQueryOptions.ts
@@ -1,13 +1,22 @@
-import { ApiResponse } from "@/models/ApiResponse";
 import { UserFavoriteProducts } from "@/models/types";
 import { getUserFavorites } from "@/services/productService";
 import { queryOptions, UseQueryOptions } from "@tanstack/react-query";
 
+// export function createGetUserFavoritesQueryOptions(
+//   options?: Omit<
+//     UseQueryOptions<ApiResponse<UserFavoriteProducts>>,
+//     "queryKey" | "queryFn"
+//   >
+// ) {
+//   return queryOptions({
+//     ...options,
+//     queryKey: ["favorites"],
+//     queryFn: getUserFavorites,
+//   });
+// }
+
 export function createGetUserFavoritesQueryOptions(
-  options?: Omit<
-    UseQueryOptions<ApiResponse<UserFavoriteProducts>>,
-    "queryKey" | "queryFn"
-  >
+  options?: Omit<UseQueryOptions<UserFavoriteProducts>, "queryKey" | "queryFn">
 ) {
   return queryOptions({
     ...options,
